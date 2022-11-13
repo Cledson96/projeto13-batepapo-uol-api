85b9be8d7faffa5519e075464c2ffeda165c347d
Cledson <cledson1996@gmail.com>
2022-11-13 12:11:29 -0300

    participants pronto

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import cors from "cors"
 import { ConnectionClosedEvent, MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(express.json());
 const mongoClient = new MongoClient("mongodb://localhost:27017");
 let db;
+let usuarios;
 
 mongoClient.connect().then(() => {
     db = mongoClient.db("uol");
@@ -25,7 +27,7 @@ const receitaSchema = joi.object({
 app.post("/participants", async (req, res) => {
     const body = req.body;
 
-    let usuarios = await db.collection("usuarios").find({}).toArray();
+    usuarios = await db.collection("usuarios").find({}).toArray();
 
     const verificador = usuarios.find(verifica => verifica.name === req.body.name)
 
@@ -46,12 +48,25 @@ app.post("/participants", async (req, res) => {
     }
     try {
         await db.collection("usuarios").insertOne({ name: req.body.name, lastStatus: Date.now() });
+        await db.collection("message").insertOne({ from: req.body.name, to: "Todos",text: 'entra na sala...',type: 'status',time: dayjs().format("HH:mm:ss") });
+
         res.status(201).send("usuario cadastrado com sucesso!");
     } catch (err) {
         res.status(500).send(err);
     }
 
 })
+
+app.get("/participants", async (req, res) => {
+
+res.send(await db.collection("usuarios").find({}).toArray());
+
+
+})
+
+
+
+
 
 app.listen(5000, () => {
     console.log(`Server running in port: ${5000}`);
