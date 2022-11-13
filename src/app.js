63ab7f1320300b4dcc442e75dbdb0a471c3616d6
Cledson <cledson1996@gmail.com>
2022-11-13 00:47:33 -0300

    dormir

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import dotenv from "dotenv";
 import joi from "joi";
 
 const app = express();
+app.use(express.json());
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
 
+mongoClient.connect().then(() => {
+	db = mongoClient.db("uol"); 
+});
 const receitaSchema = joi.object({
     name: joi.string().required().min(3).max(100),
     ingredientes: joi.string().required(),
@@ -17,7 +23,15 @@ app.post("/participants",async (req,res)=>{
 
    if(!req.body.name){
     res.status(422).send("name deve ser strings não vazio");
+    return
    }
+   try {
+    await db.collection("usuarios").insertOne({name: req.body.name , lastStatus: Date.now()});
+    res.status(201).send("usuario cadastrado com sucesso!");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+  
 })
 
 app.listen(5000, () => {
