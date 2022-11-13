1920c7e046637caf79ad82957a8d31b15e875643
Cledson <cledson1996@gmail.com>
2022-11-13 13:26:09 -0300

    almocar

--- src/app.js
+++ src/app.js
@@ -16,11 +16,6 @@ mongoClient.connect().then(() => {
 
 });
 
-const receitaSchema = joi.object({
-    name: joi.string().required().min(3).max(100),
-    ingredientes: joi.string().required(),
-    preparo: joi.string().required(),
-});
 
 
 
@@ -48,7 +43,7 @@ app.post("/participants", async (req, res) => {
     }
     try {
         await db.collection("usuarios").insertOne({ name: req.body.name, lastStatus: Date.now() });
-        await db.collection("message").insertOne({ from: req.body.name, to: "Todos",text: 'entra na sala...',type: 'status',time: dayjs().format("HH:mm:ss") });
+        await db.collection("message").insertOne({ from: req.body.name, to: "Todos", text: 'entra na sala...', type: 'status', time: dayjs().format("HH:mm:ss") });
 
         res.status(201).send("usuario cadastrado com sucesso!");
     } catch (err) {
@@ -59,11 +54,31 @@ app.post("/participants", async (req, res) => {
 
 app.get("/participants", async (req, res) => {
 
-res.send(await db.collection("usuarios").find({}).toArray());
+    res.send(await db.collection("usuarios").find({}).toArray());
 
 
 })
 
+app.post("/messages", async (req, res) => {
+    const body = req.body;
+    let valida
+    if (body.type == "message" || body.type == "private_message") {
+        valida = false;
+    } else {
+        valida = true;
+    }
+    const from = req.headers.users
+    const messageSchema = joi.object({
+        to: joi.string().required().min(1).max(100),
+        text: joi.string().required(),
+        type: joi.string().required(),
+    });
+    const validation = messageSchema.validate(body, { abortEarly: false });
+    if (validation.error || valida) {
+        res.status(422).send("Erro no envio");
+        return
+    }
+})
 
 
 
